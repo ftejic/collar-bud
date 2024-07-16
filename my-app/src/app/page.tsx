d6fa6aba68e3641c83b6920ee0b489c32af06ae5
Filip <ficatejic@gmai.com>
2024-07-16 16:37:41 +0200

    added Desc section ui

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,15 @@
+import Desc from "@/components/Desc";
 import Hero from "@/components/Hero/Hero";
 import ProductCategories from "@/components/ProductCategories/ProductCategories";
 
 export default function Home() {
   return (
-    <div className="imageBg">
-      <Hero />
-      <ProductCategories />
-    </div>
+    <>
+      <div className="imageBg">
+        <Hero />
+        <ProductCategories />
+      </div>
+      <Desc />
+    </>
   );
 }
