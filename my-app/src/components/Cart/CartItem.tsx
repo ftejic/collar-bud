184c89ca005ac0e352701d2e3eb5cee3a58df031
Filip <ficatejic@gmai.com>
2024-07-20 16:58:17 +0200

    added cart ui

---
+++ my-app/src/components/Cart/CartItem.tsx
@@ -0,0 +1,29 @@
+import { XIcon } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+function CartItem() {
+  return (
+    <div className="text-sm">
+      <div className="flex justify-between space-x-2">
+        <div className="flex space-x-2">
+          <Image
+            src={"/images/dog1.png"}
+            width={55}
+            height={55}
+            alt=""
+            className="w-14 h-14 rounded-md"
+          />
+          <div className="flex flex-col">
+            <p>Dog Collar Model 1</p>
+            <p>Size M</p>
+          </div>
+        </div>
+        <XIcon className="w-4 h-4 cursor-pointer"/>
+      </div>
+      <p className="text-center">1 x 50 USD</p>
+    </div>
+  );
+}
+
+export default CartItem;
